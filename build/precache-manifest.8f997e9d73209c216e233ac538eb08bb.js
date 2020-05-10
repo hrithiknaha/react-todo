16ddc0415f4c8d56b92c7d22866c4ecd7e1f9092
Hrithik Naha <naha.hrithkk@gmail.com>
2020-05-10 23:36:44 +0530

    Inital commit

---
+++ build/precache-manifest.8f997e9d73209c216e233ac538eb08bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d50d0b76327796b42ba635cbfebcc09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa2713facceb34dd1ec",
+    "url": "/static/css/main.4b26b95d.chunk.css"
+  },
+  {
+    "revision": "35ad327fefdc807110aa",
+    "url": "/static/js/2.b1c3182b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b1c3182b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa2713facceb34dd1ec",
+    "url": "/static/js/main.bb015928.chunk.js"
+  },
+  {
+    "revision": "a2f60d652802a3d7f776",
+    "url": "/static/js/runtime-main.5c38523e.js"
+  }
+]);
