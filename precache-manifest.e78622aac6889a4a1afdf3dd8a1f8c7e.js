db8d553f3699ae3e8fdf8ffc163c000ccb720915
Hrithik Naha <naha.hrithkk@gmail.com>
2020-05-11 00:02:13 +0530

    Updates

---
+++ precache-manifest.e78622aac6889a4a1afdf3dd8a1f8c7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65dede3fdecbe561c569597970461871",
+    "url": "https://hrithiknaha.github.io/react-todo/index.html"
+  },
+  {
+    "revision": "684194d52cb33c45f0a5",
+    "url": "https://hrithiknaha.github.io/react-todo/static/css/main.4b26b95d.chunk.css"
+  },
+  {
+    "revision": "35ad327fefdc807110aa",
+    "url": "https://hrithiknaha.github.io/react-todo/static/js/2.b1c3182b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://hrithiknaha.github.io/react-todo/static/js/2.b1c3182b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684194d52cb33c45f0a5",
+    "url": "https://hrithiknaha.github.io/react-todo/static/js/main.747926bd.chunk.js"
+  },
+  {
+    "revision": "019363234799b364121c",
+    "url": "https://hrithiknaha.github.io/react-todo/static/js/runtime-main.599f9822.js"
+  }
+]);
